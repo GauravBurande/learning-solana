4fcf0f9b658b56ae6ce77250b99bc38785204504
Gaurav <burandegaurav8899@gmail.com>
2025-03-04 14:37:11 +0530

    ✅ 08 - revoke delegate access

---
+++ 08_token-burning-and-delegation/lab/revoke-approve-tokens.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import { TRAINING_MINT_ADDRESS } from "../../07_creating-token-with-metadata/create-token-mint";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { getOrCreateAssociatedTokenAccount, revoke } from "@solana/spl-token";
+import { stringify } from "querystring";
+
+const DEVNET_URL = clusterApiUrl("devnet");
+
+const connection = new Connection(DEVNET_URL);
+const me = getKeypairFromEnvironment("ACC_1_SECRET_KEY");
+
+const revokeDelegateAccess = async (
+  mint = TRAINING_MINT_ADDRESS,
+  user = me
+) => {
+  try {
+    const userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      user,
+      mint,
+      user.publicKey
+    );
+
+    const revokeTransactionSignature = await revoke(
+      connection,
+      user,
+      userTokenAccount.address,
+      user
+    );
+
+    const explorerLink = getExplorerLink(
+      "transaction",
+      revokeTransactionSignature,
+      "devnet"
+    );
+
+    console.log(`✅ Revoke Delegate Transaction: ${explorerLink}`);
+  } catch (error) {
+    console.error(
+      `Error: ${error instanceof Error ? error.message : String(error)}`
+    );
+  }
+};
+
+revokeDelegateAccess();
+
+export default revokeDelegateAccess;
